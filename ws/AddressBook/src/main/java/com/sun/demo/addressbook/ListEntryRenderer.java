eb12999a74d0ae4fcdb0ebbddfa372e8baa50788
Thomas Lelièvre <thomaslelievre72@gmail.com>
2013-10-24 08:17:49 +0200

    TP4: debut

---
+++ ws/AddressBook/src/main/java/com/sun/demo/addressbook/ListEntryRenderer.java
@@ -0,0 +1,44 @@
+/*
+ * ListEntryRenderer.java
+ *
+ * Copyright 2006 Sun Microsystems, Inc. ALL RIGHTS RESERVED Use of 
+ * this software is authorized pursuant to the terms of the license 
+ * found at http://developers.sun.com/berkeley_license.html .
+ *
+ */
+
+package com.sun.demo.addressbook;
+
+import java.awt.Component;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JList;
+
+/**
+ *
+ * @author John O'Conner
+ *
+ */
+public class ListEntryRenderer extends DefaultListCellRenderer {
+    
+    /**
+	 * Generated seril ID.
+	 */
+	private static final long serialVersionUID = -1768098827822702989L;
+
+	/**
+     * Creates a new instance of ListEntryRenderer
+     */
+    public ListEntryRenderer() {
+    }
+
+    public Component getListCellRendererComponent(JList<?> list, Object value, 
+            int index, boolean isSelected, boolean cellHasFocus) {
+        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        
+        ListEntry entry = (ListEntry) value;
+        this.setText(entry.getName());
+        return this;
+    }
+    
+}
